a072c46e199a73706adf6a50d699174ad3c85bad
Phuc B2015003 <thienphuc4268@gmail.com>
2023-11-08 23:12:01 +0700

    update part

--- local_art_and_culture/test/widget_test.dart
+++ local_art_and_culture/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:local_art_and_culture/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
